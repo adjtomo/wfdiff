e357bfe2804663bb6ad576a887128b117ca8ce85
Vipul Silwal <vsilwal@alaska.edu>
2017-09-26 23:55:41 -0800

    add example of asdf input

--- run_wfdiff_test.py
+++ run_wfdiff_test.py
@@ -17,30 +17,26 @@ THRESHOLDS = {
     #"envelope_misfit": 0.1
     }
 
-
-# XXX : Move this specfem_util.py
-# SPECFEM encoded information in the filenames and the way this
-# is done differs depending on the SPECFEM version. This function
-# will be used to extract network id, station id, and the component
-# from any waveform file encountered in case SPECFEM ASCII output
-# is used.
-def get_net_sta_comp(filename):
-    net, sta, chan, _ = filename.split(".")
-    return net, sta, chan[-1]
-
 #--------------------------------------------------------------------------------
 # Run example
+event_file = None
+asdf_file = None
+low_res_seismos = None
+high_res_seismos = None
+stations_file = None
+event_file = None
+new_specfem_name = True
 
-iex = 1
+iex = 4
 
 if iex == 1:
     # test data (using ENZ components)
     low_res_seismos="./test_data/NGLL5/*semd"
     high_res_seismos="./test_data/NGLL7/*semd"
-    station_info="./test_data/STATIONS"
-    cmtsolution_file='./test_data/CMTSOLUTION'
+    stations_file="./test_data/STATIONS"
+    event_file='./test_data/CMTSOLUTION'
     trace_tags = ['NGLL5','NGLL7']
-    is_specfem_ascii = True
+    wf_format = 'specfem'
     output_directory = "output_test"
     output_format = 'pdf' # or ps, eps etc.
 
@@ -51,7 +47,7 @@ elif iex == 2:
     low_res_seismos="./20140831030657110_NGLL5_ENZ/*sac"
     high_res_seismos="./20140831030657110_NGLL7_ENZ/*sac"
     trace_tags = ['NGLL5-ENZ','NGLL7-ENZ']
-    is_specfem_ascii = False
+    wf_format = 'sac'
     output_directory = "output_test"
     output_format = 'pdf' # or ps, eps etc.
 
@@ -60,36 +56,50 @@ elif iex == 3:
     # Run specfem_util_test.py first to create rotate SAC file for NGLL5 and NGLL7 case
     low_res_seismos="./20140831030657110_NGLL5_RTZ/*sac"
     high_res_seismos="./20140831030657110_NGLL7_RTZ/*sac"
-    station_info="./test_data/STATIONS"
+    stations_file="./test_data/STATIONS"
     trace_tags = ['NGLL5-RTZ','NGLL7-RTZ']
-    is_specfem_ascii = False
+    wf_format = 'sac'
     output_directory = "output_test"
     output_format = 'pdf' # or ps, eps etc.
 
+elif iex == 4:
+    # Use asdf file instead (Nenana example)
+    #low_res_seismos="/store/homeglut/carltape/OUTPUT_SPECFEM3D/alaska/nenana/OUTPUT_FILES_nenana_NGLL5/*.semd"
+    #high_res_seismos="/store/homeglut/carltape/OUTPUT_SPECFEM3D/alaska/nenana/OUTPUT_FILES_nenana_NGLL7/*.semd"
+    #stations_file = '/store/homeglut/carltape/OUTPUT_SPECFEM3D/alaska/nenana/OUTPUT_FILES_nenana_NGLL5/STATIONS_FILTERED'
+    #wf_format = 'specfem'
+    low_res_seismos="./nenana_gll5.h5";
+    high_res_seismos="./nenana_gll7.h5"
+    wf_format = 'asdf'
+    trace_tags = ['gll5','gll7']  # These are SAME as the tags in the asdf file
+    output_directory = "output_test"
+    output_format = 'pdf' # or ps, eps etc.
 #--------------------------------------------------------------------------------
 
 # Configuration.
 c = WFDiff(
-    low_res_seismos=low_res_seismos, high_res_seismos=high_res_seismos, station_info=station_info,
- 
+    low_res_seismos=low_res_seismos, high_res_seismos=high_res_seismos, 
+    stations_file=stations_file,
     # Specify the units of the data and the units the analysis should take
     # place in.
     data_units="displacement", desired_analysis_units="displacement",
     # Periods to test.
     t_min=1, t_max=10, dt=1,
-    get_net_sta_comp_fct=get_net_sta_comp,
-    # Set to True if waveform ASCII files are used. All other fileformat
-    # should otherwise work just fine.
-    is_specfem_ascii=is_specfem_ascii,
     # Data window to take into account in seconds since the first sample.
-    starttime=0, endtime=100)
+    starttime=0, endtime=100,
+    new_specfem_name=new_specfem_name,
+    trace_tags=trace_tags,
+    # Set to 'specfem' if waveform ASCII files are used. 
+    # Set to 'asdf' if asdf waveform file
+    # All other fileformat should otherwise work just fine.
+    wf_format=wf_format)
 
 # Perform the frequency dependent misfit measurements. Results will be stored
 # in 'results.json' in the output directory. This file can later be read again
 # if necessary to perform the subsequent analysis.
 results = c.run(
     misfit_types=list(THRESHOLDS.keys()),
-    output_directory=output_directory, trace_tags=trace_tags,
+    output_directory=output_directory,
     # Setting this to True will create A LOT of debug plots which are very useful
     # to tune the algorithm to the problem at hand. For production runs set this to
     # False.
@@ -100,5 +110,5 @@ results = c.run(
 results.plot_all(
     output_directory=output_directory,
     thresholds=THRESHOLDS,
-    cmtsolution_file=cmtsolution_file,
+    event_file=event_file,
     output_format=output_format)
